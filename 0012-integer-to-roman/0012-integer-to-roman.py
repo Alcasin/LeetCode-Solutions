47fefc8a7c891ae0b0eb4e051c206983b28b88c4
Alper Kırca <113625368+Alcasin@users.noreply.github.com>
2025-01-28 17:40:49 +0300

    Time: 7 ms (44.82%), Space: 17.9 MB (32.65%) - LeetHub

---
+++ 0012-integer-to-roman/0012-integer-to-roman.py
@@ -0,0 +1,14 @@
+class Solution:
+    def intToRoman(self, num: int) -> str:
+        value_symbols = [
+            (1000, 'M'), (900, 'CM'), (500, 'D'), (400, 'CD'),
+            (100, 'C'), (90, 'XC'), (50, 'L'), (40, 'XL'), (10, 'X'),
+            (9, 'IX'), (5, 'V'), (4, 'IV'), (1, 'I')
+        ]
+        arr=[]
+        for value,symbol in value_symbols:
+            if num//value>0:
+                arr.append((num//value)*symbol)
+                num = num%value
+        return ''.join(arr)    
+
